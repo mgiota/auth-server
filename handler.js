295cd06d23d9d83d2f034ee50f09d626a03f9161
mgiota <giota85@gmail.com>
2019-07-12 16:29:11 +0200

    giota's code

--- handler.js
+++ handler.js
@@ -4,18 +4,20 @@ const axios = require('axios');
 
 module.exports.getAccessToken = async (event) => {
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
-    + '?client_id=t4rva9pn92dlc3v396d4u5ricj'
-    + '&client_secret=im42p66i0tensh8mcmu5nm8qjk'
+    + '?client_id=f29ecfroc9i5bv87ohotsj9pvt'
+    + '&client_secret=ul80b565abcj1gv257lcl48p18'
     + '&grant_type=authorization_code'
-    + '&redirect_uri=https://maraxai.github.io/meetup/'
+    + '&redirect_uri=https://mgiota.github.io/meetup'
     + '&code=' + event.pathParameters.code;
+    // + '&code=27b38b0e2677385529ee407d4c52ae4f';
+
 
   const info = await axios.post(MEETUP_OAUTH_URL);
   console.log(info)
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
         access_token: info.data.access_token,
